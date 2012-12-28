432a064901487be82c0ab186589c2570cd1497f5
Cameron Johnston <cameron@needle.com>
2012-12-28 01:11:40 -0700

    bump to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 maintainer        "Cameron Johnston"
 maintainer_email  "cameron@rootdown.net"
 description       "Provides a definition that sets up an ssh key and ssh wrapper script for use with deploy or deploy_revision resources"
-version           "0.0.1"
+version           "0.0.2"
