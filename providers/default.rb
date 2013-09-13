4d8239f810f1d3d60f50030f9a283d7d341514be
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-13 10:24:52 +0200

    Change mode on directory wrapper_dir only if not exist

--- providers/default.rb
+++ providers/default.rb
@@ -8,21 +8,21 @@ action :create do
   directory new_resource.ssh_key_dir do
     owner new_resource.owner
     group new_resource.group
-    mode 0740
+    mode "0740"
     recursive true
   end
 
   directory new_resource.ssh_wrapper_dir do
     owner new_resource.owner
     group new_resource.group
-    mode 0750
+    mode "0755" unless ::Dir.exists?(new_resource.ssh_wrapper_dir)
     recursive true
   end
 
   file key_path do
     owner new_resource.owner
     group new_resource.group
-    mode 0600
+    mode "0600"
     content new_resource.ssh_key_data if new_resource.ssh_key_data
   end
 
@@ -31,7 +31,7 @@ action :create do
     source "ssh_wrapper.sh.erb"
     owner new_resource.owner
     group new_resource.group
-    mode 0755
+    mode "0755"
     variables(
       :ssh_key_path => key_path,
       :app_name => new_resource.application,
